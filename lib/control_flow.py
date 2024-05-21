96001dfa3dbde057c6c8742d262422022e993912
omolo-j <omoloj135@gmail.com>
2024-05-21 13:22:20 +0300

    conditional statements

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,51 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    ADMIN_USERNAME = "admin"
+    ADMIN_PASSWORD = "12345"
+
+    if username.lower() == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+        return "Access granted"
+    else:
+        return "Access denied"
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature > 85:
+        return "It's too dang hot out there!"
+    elif temperature > 65:
+        return "It's perfect out there!"
+    elif temperature > 40:
+        return "It's a little chilly out there!"
+    elif temperature > 20:
+        return "It's brisk out there!"
+    else:
+        return "It's cold!"
+
+
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        if num2 == 0:
+            return "Error: Division by zero!"
+        else:
+            return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
+
